bc05289e10f33d84aff8f311c36a2e2cd4b2a2e6
sjava <zhu.yuanbin@gmail.com>
2026-04-15 21:48:41 +0800

    update

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -266,23 +266,9 @@ return {
     ft = "markdown",
     opts = {},
   },
-
   {
-    "dmtrKovalenko/fff.nvim",
-    build = function() require("fff.download").download_or_build_binary() end,
-    lazy = false,
-    opts = {},
-    dependencies = {
-      {
-        "AstroNvim/astrocore",
-        opts = {
-          mappings = {
-            n = {
-              ["<Leader>ff"] = { function() require("fff").find_files() end, desc = "Find files" },
-            },
-          },
-        },
-      },
-    },
+    "retran/meow.yarn.nvim",
+    dependencies = { "MunifTanjim/nui.nvim" },
+    config = function() require("meow.yarn").setup {} end,
   },
 }
