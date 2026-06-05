c3018ac1e7182fd1297a51a0bb865adc3d6d90c7
sjava <515957966@qq.com>
2026-06-05 11:26:02 +0800

    clear code

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -81,11 +81,6 @@ return {
     mappings = {
       n = {
         -- a `cond` key can provided as the string of a server capability to be required to attach, or a function with `client` and `bufnr` parameters from the `on_attach` that returns a boolean
-        gD = {
-          function() vim.lsp.buf.declaration() end,
-          desc = "Declaration of current symbol",
-          cond = "textDocument/declaration",
-        },
         ["<Leader>uY"] = {
           function() require("astrolsp.toggles").buffer_semantic_tokens() end,
           desc = "Toggle LSP semantic highlight (buffer)",
